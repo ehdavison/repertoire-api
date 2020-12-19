062fabd59856f8dc91aff06cca55d1e6714c3721
ehdavison <elijahdavison2@gmail.com>
2020-12-19 11:41:04 -0600

    Added songs model

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Song(models.Models):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=50)
+    video = models.CharField(max_length=1000)
+    tabs = models.CharField(max_length=1000)
+    notes = models.CharField(max_length=20000)
+
+    def __str__(self):
+        return f'{self.title}'
